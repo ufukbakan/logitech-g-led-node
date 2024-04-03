7d6ba3916b15c5e6aef8d65da9254358e64d46e4
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-04-03 19:46:19 +0300

    feat: eslint & husky integrations

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const { configure } = require("eslint-with-prettier");
-
-module.exports = {
-    ...configure("ts", "prettier"),
-    env: {
-        node: true,
-    },
-    ignorePatterns: ["node_modules/**/*", "dist/**/*", "**/*.json"],
-};
---
+++ eslint.config.mjs
@@ -0,0 +1,3 @@
+import { configure } from "eslint-configurer";
+
+export default configure("js", "prettier", "ts", "node");
