8da5745becbcb21b664233a86e35f09cd03e7f75
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-04-04 04:53:54 +0300

    feat: seperate type declarations

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 //@ts-check
 import typescript from "@rollup/plugin-typescript";
+import { when, format } from "rollup-plugin-by-output";
 
 export default {
     input: "src/index.ts", // Replace with your entry point
@@ -13,6 +14,13 @@ export default {
             format: "cjs", // CommonJS format for CJS
             sourcemap: true, // Include sourcemaps for debugging
         },
+        {
+            file: "dist/types/index.d.ts",
+            format: "system",
+        },
     ],
-    plugins: [typescript()],
+    plugins: [
+        typescript({ declaration: false }),
+        when(format("system"), () => typescript({ declaration: true, emitDeclarationOnly: true }))
+    ],
 };
